fb3add7191e6ab1d4465781a26b696414b7866e3
Janashell <janashell07@gmail.com>
2021-03-01 02:13:28 -0600

    Not Finished Decision Making Exercise

---
+++ decisionMakingExercise.js
@@ -0,0 +1,30 @@
+// 1a
+let randomNum = Math.random();
+randomNum *= 5;
+randomNum = Math.floor(randomNum);
+randomNum ++;
+console.log(randomNum);
+
+
+// 1b
+
+if (true) {
+    console.log("Greater than or equal to 4");
+ else if (true){
+    console.log("Equal to 2 or 3");
+}
+}
+
+// 1c
+
+
+
+
+// I'm sorry Matt, but I have sat here and messed with these first few problems for almost two hours and can not figure them out. I usually try to study and go over things during the weekend, but really didnt have a chance to this weekend due to working on my portfolio. I will definatly do that this week and do my best to understand all this better.  I just didn't want you thinking that I didn't try.  
+
+
+
+
+
+
+
